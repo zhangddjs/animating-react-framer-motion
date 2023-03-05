0042f094d3bf2d4af3b9aabc238218f02828fa86
zhangddjs <1005155946@qq.com>
2023-03-05 09:46:33 +0800

    dismiss the card item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import Modal from "./Modal";
 import Accordion from "./Accordion";
@@ -15,6 +20,7 @@ function App() {
   const [value, setValue] = useState(0);
   const [isToggled, setToggle] = useState(false);
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isCardActive, setIsCardActive] = useState(true);
   const x = useMotionValue(0);
   const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]); //坐标到-200或200，消失
 
@@ -67,17 +73,37 @@ function App() {
             <h3>Some card</h3>
             <img src={purp} />
           </Card>
-          <Card
-            drag="x"
-            dragConstraints={{
-              left: 0,
-              right: 0,
-            }}
-            style={{ x, opacity, background: "var(--blue)" }}
-          >
-            <h3>Some card</h3>
-            <img src={blue} />
-          </Card>
+          <AnimatePresence>
+            {isCardActive && (
+              <motion.div
+                exit={{ height: 0, overflow: "hidden", opacity: 0 }}
+                transition={{ opacity: { duration: 0 } }} // instantly set opacity to 0
+              >
+                <Card
+                  onDragEnd={(event, info) => {
+                    // console.log(info.point.x);
+                    if (Math.abs(info.point.x) > 200) {
+                      setIsCardActive(false);
+                    }
+                  }}
+                  drag="x"
+                  dragConstraints={{
+                    left: 0,
+                    right: 0,
+                  }}
+                  style={{
+                    x,
+                    // opacity: isCardActive ? opacity : 0,  //not correct
+                    opacity,
+                    background: "var(--blue)",
+                  }}
+                >
+                  <h3>Some card</h3>
+                  <img src={blue} />
+                </Card>
+              </motion.div>
+            )}
+          </AnimatePresence>
           <Card style={{ background: "var(--black)" }}>
             <h3>Some card</h3>
             <img src={black} />
